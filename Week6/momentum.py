5ecd63820e8581f150c9c8b7eaa1e745046868a8
Seungyong Lee <sylee0802@lunit.io>
2023-04-01 20:48:48 +0900

    fix: pct change NaN bug fix: short returns

--- Week6/momentum.py
+++ Week6/momentum.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-def cross_sectional_momentum(df, window_size=252, quantile=0.1, rebalancing_period=30):
-    returns = df.pct_change().shift(-1)
-    momentum = df.pct_change(periods=window_size)
+def cross_sectional_momentum(df, window_size=252, quantile=0.1, rebalancing_period=21):
+    returns = df.pct_change(fill_method=None).shift(-1)
+    momentum = df.pct_change(periods=window_size, fill_method=None)
 
     long_only_returns = pd.DataFrame(np.nan, index=returns.index, columns=returns.columns)
     long_short_returns = pd.DataFrame(np.nan, index=returns.index, columns=returns.columns)
@@ -14,28 +14,28 @@ def cross_sectional_momentum(df, window_size=252, quantile=0.1, rebalancing_peri
     top_half_quantile = []
     bottom_half_quantile = []
 
-    for date, row in momentum.iterrows():
+    for date, row in momentum[window_size:].iterrows():
         n = int(len(row) * quantile)
 
         if sum(~row.isna()) < n:
-            continue
+            raise ValueError(f"Number of stocks in the universe is less than {n}.")
 
         if counter % rebalancing_period == 0:
-            top_quantile = row.nlargest(n).index
-            top_half_quantile = row.nlargest(n // 2).index
-            bottom_half_quantile = row.nsmallest(n // 2).index
+            top_quantile = row.dropna().nlargest(n).index
+            top_half_quantile = row.dropna().nlargest(n // 2).index
+            bottom_half_quantile = row.dropna().nsmallest(n // 2).index
 
         # TODO: Weighted return
         long_only_returns.loc[date, :] = np.where(long_only_returns.columns.isin(top_quantile), returns.loc[date, :], np.nan)
         long_short_returns.loc[date, :] = np.where(long_short_returns.columns.isin(top_half_quantile), returns.loc[date, :], np.nan)
-        long_short_returns.loc[date, :] = np.where(long_short_returns.columns.isin(bottom_half_quantile), -returns.loc[date, :], long_short_returns.loc[date, :])
+        long_short_returns.loc[date, :] = np.where(long_short_returns.columns.isin(bottom_half_quantile), 1/(1+returns.loc[date, :])-1, long_short_returns.loc[date, :])
 
         counter += 1
 
     return long_only_returns, long_short_returns
 
-def time_series_momentum(df, window_size=252, rebalancing_period=30):
-    returns = df.pct_change()
+def time_series_momentum(df, window_size=252, rebalancing_period=21):
+    returns = df.pct_change(fill_method=None)
     rolling_mean = returns.rolling(window = window_size).mean()
     returns = returns.shift(-1)
 
@@ -56,7 +56,7 @@ def time_series_momentum(df, window_size=252, rebalancing_period=30):
 
         long_only_returns.loc[date, :] = np.where(positive_mean_returns, returns.loc[date, :], np.nan)
         long_short_returns.loc[date, :] = np.where(positive_mean_returns, returns.loc[date, :], np.nan)
-        long_short_returns.loc[date, :] = np.where(negative_mean_returns, -returns.loc[date, :], long_short_returns.loc[date, :])
+        long_short_returns.loc[date, :] = np.where(negative_mean_returns, 1/(1+returns.loc[date, :])-1, long_short_returns.loc[date, :])
 
         counter += 1
 
